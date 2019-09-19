b7c76d4c02ae036ea0584564888f0fbed59fbb9f
N150429 <37292739+N150429@users.noreply.github.com>
2019-09-19 00:51:35 -0500

    Update hexcolorcode.py

--- hexcolorcode.py
+++ hexcolorcode.py
@@ -3,3 +3,22 @@ for index in range(int(input())):
     string = input()
     x=re.findall('(#[a-fA-F0-9]{3,6})\S',string)
     [print(i) for i in x]
+
+
+    
+output:
+    
+ 11
+body {
+  color: red;
+}
+
+h1 {
+  color: #00ff00;
+}
+
+p.ex {
+  color: rgb(0,0,255);
+}
+
+#00ff00
